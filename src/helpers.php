653efcd7421f5f2464cd1bb958c34a2d44ef3d27
Nikita Babichenko <babichenkona@gmail.com>
2022-12-27 12:09:44 +0300

    minor changes

--- src/helpers.php
+++ src/helpers.php
@@ -6,7 +6,7 @@ use JetBrains\PhpStorm\NoReturn;
 if (! function_exists('redirect')) {
     #[NoReturn] function redirect(string $path, $statusCode = 303): void
     {
-        header('Location: ' . (Config::getInstance())->get('app_name') . $path, true, $statusCode);
+        header('Location: ' . (Config::getInstance())->get('app_url') . $path, true, $statusCode);
         die();
     }
 }
